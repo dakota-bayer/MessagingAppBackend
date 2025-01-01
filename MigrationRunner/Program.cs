347db3dd571ffeffc0b6205d6cc768228105ff2c
dakota-bayer <dakota.bayer@gmail.com>
2025-01-01 17:30:20 -0600

    initial commit

---
+++ MigrationRunner/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using DbUp;
+
+namespace MigrationRunner;
+
+public class Program
+{
+    static int Main(string[] args)
+    {
+        var connectionString = "Host=localhost;Port=5432;Database=messaging;User Id=postgres;Password=admin;";
+
+        // Ensure the DB exists
+        EnsureDatabase.For.PostgresqlDatabase(connectionString);
+
+        // Execute the migration scripts
+        var upgrader = DeployChanges.To
+            .PostgresqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build();
+
+        var result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(result.Error);
+            Console.ResetColor();
+            return -1;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Database upgrade successful!");
+        Console.ResetColor();
+
+        return 0;
+    }
+}
---
+++ MigrationRunner/Scripts/20241231_000_CreateMessagesTable.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+       
+CREATE TABLE IF NOT EXISTS message
+(
+    Id        UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    Content   TEXT NOT NULL,
+    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
